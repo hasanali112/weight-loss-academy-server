e4e4e21eeabfa3c74b5ee615f4b7478caa7a4fc1
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-06-09 23:13:10 +0600

    user receive from client side

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ app.use(express.json());
 
 
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_username}:${process.env.DB_password}@cluster1.4gey7ap.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,6 +26,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const userCollection = client.db('weight-loss-academy').collection('users')
+
+
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
