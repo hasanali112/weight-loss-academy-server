ac69d16ecb8cefcdd1f13af73326bbb74eb8952f
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-06-10 13:56:16 +0600

    create jwt token and receive from client side

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const app = express ();
 const cors = require ('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -28,6 +29,16 @@ async function run() {
     await client.connect();
     
     const userCollection = client.db('weight-loss-academy').collection('users')
+
+   //JWT
+   app.post('/jwt', (req, res)=>{
+     const user = req.body;
+     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+     res.send({ token })
+   })
+
+
 
     app.get('/users', async (req, res)=>{
       const result = await userCollection.find().toArray()
